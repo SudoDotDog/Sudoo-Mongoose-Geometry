cd394fe560e7063e38c0b0ae80a4ca2cc08404bc
WMXPY <wm@mengw.io>
2020-04-21 11:29:32 -0500

    implement find unit tests

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,6 @@
  * @description Index
  */
 
-export * from "./declare";
 export * from "./find";
 export * from "./schema";
 
---
+++ test/unit/find.test.ts
@@ -0,0 +1,31 @@
+/**
+ * @author WMXPY
+ * @namespace Geometry
+ * @description Declare
+ * @override Unit Test
+ */
+
+import { Coordinate, createCoordinate, createGeoJsonPoint } from "@sudoo/geometry";
+import { expect } from "chai";
+import * as Chance from "chance";
+import { createFindPointGeometryNearQuery, FindPointGeometryNearQuery } from "../../src";
+
+describe('Given [Find] helper functions', (): void => {
+
+    const chance: Chance.Chance = new Chance('geometry-find');
+
+    it('should be able to create find query', (): void => {
+
+        const coordinate: Coordinate = createCoordinate(chance.integer(), chance.integer());
+        const distance: number = chance.integer();
+
+        const query: FindPointGeometryNearQuery = createFindPointGeometryNearQuery(coordinate, distance);
+
+        expect(query).to.be.deep.equal({
+            $near: {
+                $maxDistance: distance,
+                $geometry: createGeoJsonPoint(coordinate),
+            },
+        });
+    });
+});
